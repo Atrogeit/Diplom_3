42ceb22e52ca60f42cdf24b022806d528a3e0c02
Egor Zakharov <zakh.egor@gmail.com>
2023-02-18 00:03:19 +0300

    Added additional check into 'checkRegistrationIsSuccessful' test, updated allure-results

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -2,11 +2,12 @@ import User.*;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.Before;
 import org.junit.Test;
-import pages.AuthorizationPage;
-import pages.LoginSteps;
-import pages.PasswordRecoveryPage;
-import pages.RegistrationPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import pages.*;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static tools.URLs.*;
 
 public class RegistrationTest extends TestSetUp {
@@ -17,12 +18,14 @@ public class RegistrationTest extends TestSetUp {
     private LoginSteps loginSteps;
     private User user;
     private final String ERROR_TEXT = "Некорректный пароль";
+    private MainPage mainPage;
 
     @Before
     public void setUp() {
         super.setUp();
         driver.get(REGISTRATION_PATH);
         user = UserGenerator.getUser();
+        mainPage = new MainPage(driver, authorizationPage);
         registrationPage = new RegistrationPage(driver);
         loginSteps = new LoginSteps(driver, authorizationPage, registrationPage);
         passwordRecoveryPage = new PasswordRecoveryPage(driver);
@@ -40,6 +43,14 @@ public class RegistrationTest extends TestSetUp {
         String actualRegistrationUrl = driver.getCurrentUrl();
 
         assertEquals(expectedRegistrationUrl, actualRegistrationUrl);
+
+        authorizationPage.userAuthorization(user.getEmail(), user.getPassword());
+        mainPage.waitForCreateOrderButton();
+
+        WebElement orderButton = driver.findElement(By.xpath("//button[text()='Оформить заказ']"));
+        boolean orderButtonIsVisible = orderButton.isDisplayed();
+
+        assertTrue(orderButtonIsVisible);
     }
 
     @Test
