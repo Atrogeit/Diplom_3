42c8f67e24f5450e3e6cfbb5e4009b2add941c4c
Egor Zakharov <zakh.egor@gmail.com>
2023-02-20 17:35:46 +0300

    Added asserts to TabSwitchTests, updated allure-results

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -13,10 +13,10 @@ public class MainPage{
     public By loginButton = By.xpath("//button[text()='Войти в аккаунт']");
     public By createOrderButton = By.xpath("//button[text()='Оформить заказ']");
     public By constructorTabTitle = By.xpath("//h1[text()='Соберите бургер']");
-    public By bunsTab = By.xpath(".//span[@class = 'text text_type_main-default' and text() = 'Булки']");
+    public By bunsTab = By.xpath(".//div[@class = 'tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect']/span[@class = 'text text_type_main-default' and text() = 'Булки']");
 
-    public By sauceTab = By.xpath(".//span[@class = 'text text_type_main-default' and text() = 'Соусы']");
-    public By fillingsTab = By.xpath(".//span[@class = 'text text_type_main-default' and text() = 'Начинки']");
+    public By sauceTab = By.xpath(".//div[@class = 'tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect']/span[@class = 'text text_type_main-default' and text() = 'Соусы']");
+    public By fillingsTab = By.xpath(".//div[@class = 'tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect']/span[@class = 'text text_type_main-default' and text() = 'Начинки']");
 
 
     public MainPage(WebDriver driver, AuthorizationPage authorizationPage) {
@@ -39,16 +39,22 @@ public class MainPage{
 
     @Step("bunsTab is clicked")
     public void clickBunsTab(){
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.visibilityOfElementLocated(bunsTab));
         driver.findElement(bunsTab).click();
     }
 
     @Step("sauceTab is clicked")
     public void clickSauceTab(){
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.visibilityOfElementLocated(sauceTab));
         driver.findElement(sauceTab).click();
     }
 
     @Step("fillingsTab is clicked")
     public void clickFillingsTab(){
+        new WebDriverWait(driver, 5)
+                .until(ExpectedConditions.visibilityOfElementLocated(fillingsTab));
         driver.findElement(fillingsTab).click();
     }
 
--- src/test/java/TabSwitchTest.java
+++ src/test/java/TabSwitchTest.java
@@ -3,16 +3,14 @@ import io.qameta.allure.junit4.DisplayName;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import pages.*;
 
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.assertEquals;
 import static tools.URLs.URL;
 
 public class TabSwitchTest extends TestSetUp {
     private AuthorizationPage authorizationPage;
-    private LoginSteps loginSteps;
-    private RegistrationPage registrationPage;
     private MainPage mainPage;
 
 
@@ -20,45 +18,49 @@ public class TabSwitchTest extends TestSetUp {
     public void setUp() {
         super.setUp();
         driver.get(URL);
-        registrationPage = new RegistrationPage(driver);
-        loginSteps = new LoginSteps(driver, authorizationPage, registrationPage);
-        authorizationPage = new AuthorizationPage(driver, loginSteps);
         mainPage = new MainPage(driver, authorizationPage);
     }
 
     @Test
     @DisplayName("Switching to 'Buns Tab'")
-    public void checkSwitchToBunsTabScreen() {
+    public void checkSwitchToBunsTabScreenAndReturnsCorrectCurrentTabTitle() {
 
         mainPage.clickSauceTab();
         mainPage.clickBunsTab();
-        //Тут идет проверка по элементу, в случае, если выбрана не та вкладка, то тест упадет на поиске элемента
-        WebElement  bunTabIsCurrent = driver.findElement(By.xpath(".//div[@class= 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']/span[@class = 'text text_type_main-default' and text() = 'Булки']"));
-        boolean bunTabIsVisible = bunTabIsCurrent.isDisplayed();
-        assertTrue(bunTabIsVisible);
+
+        //Используем локатор текущей выбранной вкладки без привязки к наименованию, для проверки имени вкладки.]
+        //Если кликнуть по другой вкладке, то getText вернет другое название вкладки
+        String actualTabText = driver.findElement(By.xpath(".//div[@class= 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']")).getText();
+        String expectedTabText = "Булки";
+
+        assertEquals(expectedTabText, actualTabText);
+
     }
 
     @Test
     @DisplayName("Switching to 'Sauce Tab'")
-    public void checkSwitchToSauceTabScreen() {
+    public void checkSwitchToSauceTabScreenAndReturnsCorrectCurrentTabTitle() {
 
-        mainPage.clickFillingsTab();
         mainPage.clickSauceTab();
-        //Тут идет проверка по элементу, в случае, если выбрана не та вкладка, то тест упадет на поиске элемента
-        WebElement  sauceTabIsCurrent = driver.findElement(By.xpath(".//div[@class= 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']/span[@class = 'text text_type_main-default' and text() = 'Соусы']"));
-        boolean sauceTabIsVisible = sauceTabIsCurrent.isDisplayed();
-        assertTrue(sauceTabIsVisible);
+
+        //Используем локатор текущей выбранной вкладки без привязки к наименованию, для проверки имени вкладки.]
+        //Если кликнуть по другой вкладке, то getText вернет другое название вкладки
+        String actualTabText = driver.findElement(By.xpath(".//div[@class= 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']")).getText();
+        String expectedTabText = "Соусы";
+
+        assertEquals(expectedTabText, actualTabText);
     }
 
     @Test
     @DisplayName("Switching to 'Filling Tab'")
-    public void checkSwitchToFillingTabScreen() {
+    public void checkSwitchToFillingTabScreenAndReturnsCorrectCurrentTabTitle() {
 
-        mainPage.clickSauceTab();
         mainPage.clickFillingsTab();
-        //Тут идет проверка по элементу, в случае, если выбрана не та вкладка, то тест упадет на поиске элемента
-        WebElement  fillingsTabIsCurrent = driver.findElement(By.xpath(".//div[@class= 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']/span[@class = 'text text_type_main-default' and text() = 'Начинки']"));
-        boolean fillingTabIsVisible = fillingsTabIsCurrent.isDisplayed();
-        assertTrue(fillingTabIsVisible);
+        //Используем локатор текущей выбранной вкладки без привязки к наименованию, для проверки имени вкладки.]
+        //Если кликнуть по другой вкладке, то getText вернет другое название вкладки
+        String actualTabText = driver.findElement(By.xpath(".//div[@class= 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']")).getText();
+        String expectedTabText = "Начинки";
+
+        assertEquals(expectedTabText, actualTabText);
     }
 }
